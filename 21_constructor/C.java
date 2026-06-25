789585f6ed92278fbbc5a203b8f7507b006ba091
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-25 09:29:30 +0530

    Added folder 21

---
+++ 21_constructor/C.java
@@ -0,0 +1,17 @@
+class C {
+
+    C (int y) {
+
+       System.out.println("namaste");
+
+    }
+
+         public static void main(String[] args) {
+
+              C x = new C(15);
+     
+            System.out.println(x);
+
+         }
+     
+}
