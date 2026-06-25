30f19f5372f7b7b78a6453bf24bc067726c8c545
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-25 09:30:27 +0530

    Added folder 21

---
+++ 21_constructor/E.java
@@ -0,0 +1,25 @@
+class E {
+
+     E () {
+
+         System.out.println("MAYANK");
+     }
+
+      E (int y) {
+   
+       System.out.println("AGRAWAL");
+         
+      }
+
+        public static  void main(String[] args) {
+
+             E x = new E();           // har ek obj ka pass aap jitna overloaded constructors banayoga woh rehanga..
+
+              E y = new E();
+
+                // System.out.println(x);
+                //   System.out.println(y);
+
+
+        }
+}
