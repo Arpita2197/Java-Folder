49be975975d1b8480ccca1de37875df666fb90bf
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-27 09:35:37 +0530

    Added Folder 22

---
+++ 22_constructors/Z5.java
@@ -0,0 +1,18 @@
+class Z5 {
+
+     Z5(int y) {
+
+         this();   super();
+
+     }
+
+     Z5() {
+
+
+     }
+}
+
+//  Z5.java:5: error: call to super must be first statement in constructor
+//          this();   super();
+//                         ^
+// 1 error
