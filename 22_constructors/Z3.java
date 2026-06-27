76ea26fca62d49c8006f2ff0b525cd57cf43dda8
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-27 09:35:15 +0530

    Added Folder 22

---
+++ 22_constructors/Z3.java
@@ -0,0 +1,19 @@
+class Z3 {
+
+     Z3(int y) {
+
+         super();
+         this();
+
+     }
+
+     Z3() {
+
+
+     }
+}
+
+//  Z3.java:6: error: call to this must be first statement in constructor
+//          this();
+//              ^
+// 1 error
