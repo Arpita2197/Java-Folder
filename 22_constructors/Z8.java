58b61777bd57b14e5ee29a0af31fb007408c2cd7
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-27 09:36:15 +0530

    Added Folder 22

---
+++ 22_constructors/Z8.java
@@ -0,0 +1,24 @@
+class Z8 {
+
+    final int x;
+
+     Z8() {
+
+        super();
+
+        x=9;
+
+     }
+
+     Z8(int y) {
+
+         x = y;
+     }
+
+     public static void main(String[] args) {
+
+         Z8 y = new Z8();
+
+         System.out.println(y.x);
+     }
+}
