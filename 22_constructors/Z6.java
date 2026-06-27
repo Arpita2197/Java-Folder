5d22e76c251eb6fecbfc8b8a690e36cce72a28e8
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-27 09:35:48 +0530

    Added Folder 22

---
+++ 22_constructors/Z6.java
@@ -0,0 +1,15 @@
+class Z6 {
+
+     Z6() {
+
+         this();
+     }
+}
+
+//  C:\Users\lenovo\OneDrive\Desktop\java\22_constructors>javac Z6.java
+// Z6.java:3: error: recursive constructor invocation
+//      Z6() {
+//      ^
+// 1 error
+
+  
