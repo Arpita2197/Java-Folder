660feb96d882dbc592b5d00d9dc93303e5ba00a5
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-27 09:35:25 +0530

    Added Folder 22

---
+++ 22_constructors/Z4.java
@@ -0,0 +1,20 @@
+class Z4 {
+
+     Z4(int y) {
+
+         this();
+         super();
+
+     }
+
+     Z4() {
+
+
+     }
+}
+
+//  Z4.java:6: error: call to super must be first statement in constructor
+//          super();
+//               ^
+// 1 error
+
