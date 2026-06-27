4255eac75e14c95fdb2c642172d321b47e9ec9ee
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-27 09:36:38 +0530

    Added Folder 22

---
+++ 22_constructors/Z10.java
@@ -0,0 +1,19 @@
+class CC {
+
+    private CC() {
+
+    }
+}
+
+class Z10 extends CC {
+
+    Z10() {
+
+
+    }
+}
+
+// Z10.java:10: error: CC() has private access in CC
+//     Z10() {
+//           ^
+// 1 error
