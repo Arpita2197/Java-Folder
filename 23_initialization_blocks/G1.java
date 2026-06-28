196465524c893a4bdafe5e14934f4c94adfb9cdb
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-28 09:29:14 +0530

    Added folder 23

---
+++ 23_initialization_blocks/G1.java
@@ -0,0 +1,40 @@
+class G1 {
+
+     // anymous code sara constructor ma call hoga....
+
+     {
+        System.out.println("S");
+     }
+ 
+      {
+        System.out.println("A");
+     }
+
+      {
+        System.out.println("H");
+     }
+
+      {
+        System.out.println("U");
+     }
+
+       G1 () {
+
+           System.out.println("m");
+       }
+
+        G1(int x) {
+
+          System.out.println("e");
+             
+        }
+         
+      public static void main (String[] args) {
+
+         G1 x = new G1();
+         G1 y = new G1(1);
+      }
+}
+
+
+      
