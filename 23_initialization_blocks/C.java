450ed451d5d4578fcacd32d8ccfaf91a9f8eb6de
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-28 09:28:08 +0530

    Added folder 23

---
+++ 23_initialization_blocks/C.java
@@ -0,0 +1,23 @@
+class C {
+
+     {
+              // instance level...
+           System.out.println("ARPITA");
+         
+     }
+
+        public static void main(String[] args) {
+
+                   System.out.println("start");
+
+                         C x = new C();
+
+                          C y = new C();
+
+                           C z = new C();
+
+                       System.out.println("end");
+
+
+              }
+}
