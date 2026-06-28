e5c0f6a60a92ed6abf6ca8c1fd4e359757d2c484
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-28 09:28:58 +0530

    Added folder 23

---
+++ 23_initialization_blocks/G.java
@@ -0,0 +1,29 @@
+class G {
+
+      // anynomus code  "TOP-DOWN" approch ma print hota ha.....
+
+     {
+        System.out.println("S");
+     }
+ 
+      {
+        System.out.println("A");
+     }
+
+      {
+        System.out.println("H");
+     }
+
+      {
+        System.out.println("U");
+     }
+
+      public static void main (String[] args) {
+
+         G x = new G();
+      }
+
+
+
+      
+}
