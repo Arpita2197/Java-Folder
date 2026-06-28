89c41079ba774061cca78ff71871b0239428cffd
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-28 09:27:55 +0530

    Added folder 23

---
+++ 23_initialization_blocks/B.java
@@ -0,0 +1,21 @@
+class B {
+
+     {
+           System.out.println("namasteee");
+
+     }
+
+              public static void main(String[] args) {
+
+                   System.out.println("good");
+
+                 B x;
+                 B y;
+                 B z ;
+
+                       System.out.println("bad");
+
+
+              }
+
+}
