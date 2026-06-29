561fa403ec4d1c3c94253d15a3340342d0b6fbe8
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-29 07:25:32 +0530

    Added folder 24

---
+++ 24_arrays/M.java
@@ -0,0 +1,17 @@
+class M {
+     
+     public static void main(String[] args) {
+
+         String[] x = new String[] {"chotaBheem", "chutki","kaliya","dholu"}; // short-cut trick....
+
+          System.out.println(x.length);
+
+           System.out.println("========");
+
+            for(int i=0;i<x.length;i++) {
+
+                 System.out.println(x[i]);
+            }
+
+     }
+}
