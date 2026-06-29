ff0d8c5ca07cafd40cd0b649e1dcd8c378594608
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-29 07:22:23 +0530

    Added folder 24

---
+++ 24_arrays/B.java
@@ -0,0 +1,11 @@
+class B {
+
+     //  AN ARRAY VARIABLE IS A REFERENCE VARIABLE AND WILL BE IMPLICITLY INITIALIZE WITH ( NULL VALUE)....
+      static int [] x ;
+
+      public static void main (String[] args) {
+
+          System.out.println(x);
+
+      }
+}
