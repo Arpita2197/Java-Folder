2ac89ce2237822e0e01eb29194f08d2e16fe1f8d
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-29 07:23:05 +0530

    Added folder 24

---
+++ 24_arrays/F.java
@@ -0,0 +1,17 @@
+class F {
+
+     public static void main(String[] args) {
+
+         int[] x = new int[3];                  // int = premitive thats why 0 value is implicitly assigned...
+
+          System.out.println(x.length);
+
+          System.out.println("~~~~~~~~~~");
+
+            System.out.println(x[0]);      // 0
+           System.out.println(x[1]);       // 0
+            System.out.println(x[2]);      // 0
+
+           
+     }
+}
