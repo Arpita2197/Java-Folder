f15c869a7f52f3a411bb081830344544e6e54faf
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-29 07:23:15 +0530

    Added folder 24

---
+++ 24_arrays/G.java
@@ -0,0 +1,17 @@
+class G {
+
+     public static void main(String[] args) {
+
+         String[] x = new String[3];           // String = non-premitive thats why null value is implicitly assigned...
+
+          System.out.println(x.length);
+
+          System.out.println("~~~~~~~~~~");
+
+            System.out.println(x[0]);       // null
+           System.out.println(x[1]);        // null 
+            System.out.println(x[2]);       // null
+
+           
+     }
+}
