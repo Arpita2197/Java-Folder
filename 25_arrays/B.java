84e3440b4db671358090cb14d0a90910612ef8c4
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-30 09:22:02 +0530

    Added folder 25

---
+++ 25_arrays/B.java
@@ -0,0 +1,14 @@
+class B {
+
+    public static void main(String[] args) {
+
+         int [] x = new int[0];
+
+          System.out.println(x.length);   //0
+
+           System.out.println(x[0]);
+    }
+}
+
+//  Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 0 out of bounds for length 0
+//         at B.main(B.java:9)
