5c3d3335a7e3239f45173af9ffe271b780fd60e3
Arpita2197 <arpitasahu2197@gmail.com>
2026-06-30 09:22:27 +0530

    Added folder 25

---
+++ 25_arrays/C.java
@@ -0,0 +1,19 @@
+class C {
+
+  public static void main(String[] args) {
+
+     char a ='A';
+     byte b = 11;
+     short c = 229;
+     int d = 1000;
+
+      int [] x = {a,b,c,d};
+
+
+      for(int next : x)
+
+       System.out.println(next);
+
+  }
+     
+}
