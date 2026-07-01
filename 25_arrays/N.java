8b6dafd9c5bc20a13025a113594527b1b93c2e1f
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-01 09:39:08 +0530

    Added folder 25

---
+++ 25_arrays/N.java
@@ -0,0 +1,17 @@
+class N {
+
+     public static void main(String[] args) {
+
+         char[] x = {'m', 'a', 'n', 'o' ,'j'};
+
+          for (char nx : x) {
+
+             System.out.println(nx); 
+
+          }
+
+             System.out.println();
+
+               System.out.println(x);   // ref code ki jagahh name hee print hua.....
+     }
+}
