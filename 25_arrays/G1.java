be569578c13289d62748661b36e9d7c5a3c441e4
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-01 09:37:33 +0530

    Added folder 25

---
+++ 25_arrays/G1.java
@@ -0,0 +1,15 @@
+class G1 {
+
+
+    public static void main(String[] args) {
+
+         int [] x = new byte[5];
+
+ }
+     
+}
+
+//  G1.java:6: error: incompatible types: byte[] cannot be converted to int[]
+//          int [] x = new byte[5];
+//                     ^
+// 1 error
