a0e181d786e6beda33f7fa8c83aed29ddf077dd2
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-01 09:40:01 +0530

    Added folder 25

---
+++ 25_arrays/P.java
@@ -0,0 +1,19 @@
+          // 2D Array
+ class P {
+
+     public static void main(String[] args) {
+        int[][] x = new int[4][3];
+
+         System.out.println(x);
+         System.out.println("~~~~~~~~~~~~");
+         System.out.println(x[0]);
+         System.out.println(x[1]);
+         System.out.println(x[2]);
+         System.out.println(x[3]);
+         System.out.println("~~~~~~~~~~~~");
+         System.out.println(x[0][0]);
+         System.out.println(x[0][1]);
+         System.out.println(x[0][2]);
+
+     } 
+ }         
