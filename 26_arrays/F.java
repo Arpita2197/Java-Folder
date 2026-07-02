f65dd6d0d2e940a1ada01272e6c3bdd044a3eb4f
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-02 09:28:37 +0530

    Added folder 26

---
+++ 26_arrays/F.java
@@ -0,0 +1,22 @@
+class F {
+
+    public static void main(String[] args){
+        int[][][] x = new int[4][3][2];
+
+          System.out.println(x);
+          System.out.println("~~~~~~~~~~~~~");
+          System.out.println(x[0]);
+          System.out.println(x[1]);
+          System.out.println(x[2]);
+          System.out.println("~~~~~~~~~~~~~");
+          System.out.println(x[0][0]);
+          System.out.println(x[0][1]);
+          System.out.println(x[0][2]);
+          System.out.println("~~~~~~~~~~~~~");
+          System.out.println(x[0][0][0]);
+          System.out.println(x[0][0][1]);
+         
+
+
+    }
+}                    
