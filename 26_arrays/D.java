2212bc3fb6e72dee6e9b7aeb2e71cb8ea26ea271
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-02 09:27:58 +0530

    Added folder 26

---
+++ 26_arrays/D.java
@@ -0,0 +1,14 @@
+class D {
+    public static void main(String[] args){
+        int[][] x = new int[][]{{10,11,12,13},{2},{20,21,22,23,24,334},{2,4,5}};
+
+         for(int i=0;i<x.length;i++){
+            for(int j=0;j<x[i].length;j++){
+                System.out.println(x[i][j]);
+            }
+
+             System.out.println("~~~~~~~~~~~~~");
+
+         }
+    }
+}
