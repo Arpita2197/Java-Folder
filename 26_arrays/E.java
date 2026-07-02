33de3c4e5d8955f88fa5d0db7374b33500bf9acf
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-02 09:28:08 +0530

    Added folder 26

---
+++ 26_arrays/E.java
@@ -0,0 +1,18 @@
+                    // 3D Array
+class E {
+
+    public static void main(String[] args){
+        int[][][] x = new int[4][3][2];
+
+          System.out.println(x.length);
+          System.out.println("~~~~~~~~~~~~~");
+          System.out.println(x[0].length);
+          System.out.println(x[1].length);
+          System.out.println(x[2].length);
+          System.out.println("~~~~~~~~~~~~~");
+          System.out.println(x[0][0].length);
+          System.out.println(x[0][1].length);
+          System.out.println(x[0][2].length);
+
+    }
+}                    
