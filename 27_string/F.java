19a3a8009d4d1532937b67ecd179a9794cef9ef4
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-03 09:28:12 +0530

    Added folder 27

---
+++ 27_string/F.java
@@ -0,0 +1,10 @@
+class F {
+
+     public static void main(String[] args) {
+
+         String a = "raju";
+         String b = new String("raju");
+
+            System.out.println(a==b);
+     }
+}
