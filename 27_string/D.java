30ba8181ab07f5ee6b667d5f54b254e6dfce385e
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-03 09:27:27 +0530

    Added folder 27

---
+++ 27_string/D.java
@@ -0,0 +1,18 @@
+class D {
+
+      public static void main(String[] args) {
+       
+        String a = "om";
+               
+        String b = "om";
+               
+        String c = "om";
+
+
+          System.out.println(a==b);            // == operator ==> sirf ref.var check karta hai...
+          System.out.println(b==c);
+          System.out.println(a==c);
+
+         
+      }
+}
