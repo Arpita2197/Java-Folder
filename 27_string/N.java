09f0b57c8e75c30769aea3c3c68ff85dd2b7cfc4
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-04 17:00:25 +0530

    Added folder 27

---
+++ 27_string/N.java
@@ -0,0 +1,12 @@
+class N {
+
+     public static void main(String[] args) {
+
+         String a = "aman";
+
+          System.out.println(a.indexOf('a'));
+          System.out.println(a.indexOf('m'));
+          System.out.println(a.indexOf('a'));
+          System.out.println(a.indexOf('n'));
+     }
+}
