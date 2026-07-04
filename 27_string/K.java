e529ea319e677563a278dd7398c8844f53ebbad8
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-04 16:59:06 +0530

    Added folder 27

---
+++ 27_string/K.java
@@ -0,0 +1,14 @@
+class K {
+
+     public static void main(String[] args) {
+
+         String y = "mohan";
+
+          System.out.println(y.charAt(0));
+          System.out.println(y.charAt(1));
+          System.out.println(y.charAt(2));
+          System.out.println(y.charAt(3));
+          System.out.println(y.charAt(4));
+       
+     }
+}
