c5fae3ad1ce0766b08f99f40a75daa588d1f080a
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-04 07:12:30 +0530

    Added folder 27

---
+++ 27_string/J.java
@@ -0,0 +1,16 @@
+class J {
+
+     public static void main(String[] args) {
+
+
+       String x ="mohan";
+        
+          char a =  x.charAt(2);
+
+           System.out.println(a);
+
+
+
+
+     }
+}
