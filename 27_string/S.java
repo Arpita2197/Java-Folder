997588b74dd161c1633c61f624b944fa800eb465
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-05 12:02:37 +0530

    Added folder 27

---
+++ 27_string/S.java
@@ -0,0 +1,12 @@
+class S {
+
+     public static void main(String[] args) {
+
+         String x = "om is a good boy and he is my best friend and he is in class 10th";
+
+         System.out.println(x.indexOf("are"));
+         
+     }
+}
+
+ // o/p == -1
