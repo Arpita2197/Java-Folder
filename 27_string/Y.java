c8807a5e7c187c15e6a16ee6c2a0a6ab5863192f
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-10 07:48:03 +0530

    Added Folder 27

---
+++ 27_string/Y.java
@@ -0,0 +1,11 @@
+class Y {
+
+     public static void main(String[] args) {
+
+        //  String x = "mohanjodaro"
+         String x ="";
+
+          System.out.println(x.length());
+              System.out.println(x.isEmpty());
+     }
+}
