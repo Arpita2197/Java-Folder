fb4110e03e0d2a097a52a4732531a024aa0daf1c
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-15 09:37:33 +0530

    Added folder 28

---
+++ 28_string/A.java
@@ -0,0 +1,19 @@
+class A {
+    
+    public static void main(String[] args) {
+        String x = "mohan";
+        String y = "mihan";
+
+         String z = x.replace('o','i');   // replace==> ka obj non-pool ma banta ha
+
+          System.out.println(x);
+          System.out.println(y);
+          System.out.println(z);
+          System.out.println(y==z);
+           
+    }
+    
+ }    
+
+     
+
