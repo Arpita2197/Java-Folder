236b9661d69e4762771e611a70f641b0a4f567e9
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-15 09:39:40 +0530

    Added folder 28

---
+++ 28_string/G.java
@@ -0,0 +1,17 @@
+class G {
+    public static void main(String[] args) {
+        String x = "mohanjodaro";
+
+        String y = x.toUpperCase();
+
+        
+        System.out.println(x);
+
+        System.out.println(y);
+
+
+    }
+
+}
+
+
