cf9eb5212d71214ae6aaa7ea07e22bd2006f478f
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-16 09:17:39 +0530

    Added folder 28

---
+++ 28_string/P.java
@@ -0,0 +1,16 @@
+class P {
+
+    public static void main(String[] args) {
+
+        String x = new String("mohan");
+         String y = new String("mohan");
+          String z = new String("moHaN");
+
+          System.out.println(x.equals(y));   
+
+         System.out.println(x.equals(z));
+
+    }
+
+}
+
