ba3329d0c6fd15f33a857db5a2fb0cc3dd9544d8
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-16 09:16:16 +0530

    Added folder 28

---
+++ 28_string/L.java
@@ -0,0 +1,16 @@
+class L {
+    
+    public static void main(String[] args) {
+        String x = "mohanjodaro";
+
+        // boolean a = x.startsWith("mohan");
+
+         boolean a = x.startsWith("ram");
+
+        System.out.println(a);
+        
+    }
+
+    
+}
+
