4dc209aaf83c02aa9d3d1e67643eba43bcdc9b1e
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-16 09:16:28 +0530

    Added folder 28

---
+++ 28_string/M.java
@@ -0,0 +1,12 @@
+class M {
+    public static void main(String[] args) {
+        String x = "rampur";
+        
+        //   boolean a = x.endsWith("pur");
+
+                  boolean a = x.endsWith("bad");
+         
+        System.out.println(a);
+
+    }
+}
