d77750457098cd3d1097e2ea8807ede023dc29ea
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-16 09:17:30 +0530

    Added folder 28

---
+++ 28_string/O1.java
@@ -0,0 +1,19 @@
+class O1 {
+
+    public static void main(String[] args) {
+        String x = "mohan";
+
+       // String y = x + "  kumar";
+
+       //String y = x + "true";
+ 
+       String y = x + 12;
+
+
+          System.out.println(x);
+          System.out.println(y);
+
+
+    }
+
+}
