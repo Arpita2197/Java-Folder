42142b0615e687ee6dee7c15757321ee8dcc7ee3
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-16 09:16:53 +0530

    Added folder 28

---
+++ 28_string/N.java
@@ -0,0 +1,15 @@
+class N {
+
+    public static void main(String[] args) {
+        String x = "paramcomputer";
+        
+         // boolean f = x.contains("ram");
+
+        //  boolean f = x.contains("computer");
+
+        boolean f = x.contains("yamraj");
+         
+        System.out.println(f);
+
+    }
+}
