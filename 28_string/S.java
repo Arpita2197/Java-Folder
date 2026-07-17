c895ce194db91e91b3a1b9c8783c71ced2e359a1
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-17 19:49:02 +0530

    Added folder 28

---
+++ 28_string/S.java
@@ -0,0 +1,16 @@
+class S {
+
+    public static void main(String[] args) {
+        String x = "mohan";
+
+         System.out.println(x);
+
+          x = "ganesh";
+
+           System.out.println(x);
+
+
+    }
+
+}
+
