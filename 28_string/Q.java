5f0d64981df23ed27ad7898eee557b7b43db2061
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-17 19:47:06 +0530

    Added folder 28

---
+++ 28_string/Q.java
@@ -0,0 +1,19 @@
+class Q {
+
+     public static void main(String[] args) {
+
+         String x = "My name is :" + args[0] + "and My age is :" + args[1] + " and My marks: " + args[2];
+
+
+          System.out.println(x);
+     }
+}
+
+
+//  C:\Users\lenovo\Desktop\java\28_string>java Q
+// Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 0 out of bounds for length 0
+//         at Q.main(Q.java:5)
+
+
+
+//   RUN - TIME EXCEPTION.... 
