ef9e97f82d8c9aa57e5104cda6d64ec8a650aab7
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-18 20:40:44 +0530

    Added folder 29

---
+++ 29_wrapper_classes/C2.java
@@ -0,0 +1,17 @@
+class C2 {
+
+     public static void main(String[] args) {
+
+
+       Byte a = new Byte("12");
+
+       System.out.println(a);   
+
+         
+     }
+}
+
+//  C2.java:6: warning: [removal] Byte(String) in Byte has been deprecated and marked for removal
+// //        Byte a = new Byte("12");
+//                 ^
+// 1 warning
