b680cfcebed04a21d3c2259a46b4f305c6d812fa
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-18 20:38:06 +0530

    Added folder 29

---
+++ 29_wrapper_classes/A.java
@@ -0,0 +1,17 @@
+class A {
+
+     public static void main(String[] args) {
+
+         Boolean x = new Boolean("true");
+
+          System.out.println(x);
+     }
+}
+
+
+//    1 ==> WARNING...
+
+//  A.java:5: warning: [removal] Boolean(String) in Boolean has been deprecated and marked for removal
+//          Boolean x = new Boolean("true");
+//                      ^
+// 1 warning
