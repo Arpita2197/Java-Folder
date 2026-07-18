f2843512aff50a2ac57219dd82dc61cea3dd993f
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-18 20:40:12 +0530

    Added folder 29

---
+++ 29_wrapper_classes/C1.java
@@ -0,0 +1,17 @@
+class C1 {
+
+     public static void main(String[] args) {
+
+          byte x = 127;
+       Byte a = new Byte(x);
+
+       System.out.println(a);   
+
+         
+     }
+}
+
+//  C1.java:6: warning: [removal] Byte(byte) in Byte has been deprecated and marked for removal
+//        Byte a = new Byte(x);
+//                 ^
+// 1 warning
