1f680fb059f5fdafe80287bb40cde98c2ffbdf2a
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-18 20:38:56 +0530

    Added folder 29

---
+++ 29_wrapper_classes/B.java
@@ -0,0 +1,18 @@
+
+class B {
+
+     public static void main(String[] args) {
+
+       Character a = new Character ('E');
+
+       System.out.println(a);   // outpu == E (wrapped value)
+
+         
+     }
+}
+
+//  B.java:5: warning: [removal] Character(char) in Character has been deprecated and marked for removal
+//        Character a = new Character ('E');
+//                      ^
+// 1 warning
+
