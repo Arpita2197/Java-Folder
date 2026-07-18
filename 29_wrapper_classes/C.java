aea53fd704690c4e34a9dbf70f8380ff1689c4cf
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-18 20:39:48 +0530

    Added folder 28

---
+++ 29_wrapper_classes/C.java
@@ -0,0 +1,17 @@
+class C {
+
+     public static void main(String[] args) {
+
+          byte x = 23;
+       Byte a = new Byte(x);
+
+       System.out.println(a);   
+
+         
+     }
+}
+
+//  C.java:6: warning: [removal] Byte(byte) in Byte has been deprecated and marked for removal
+//        Byte a = new Byte(x);
+//                 ^
+// 1 warning
