f62d1d1658559b0fa61bc6ab2176e86fea5f37e9
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-19 22:48:58 +0530

    Added folder 29

---
+++ 29_wrapper_classes/F2.java
@@ -0,0 +1,12 @@
+class F2 {
+
+    
+    public static void main(String[] args) { 
+
+        Integer i = Integer.valueOf("27",8);    // 8 ==> octal number
+
+        System.out.println(i);
+
+    }
+
+}
