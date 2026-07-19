2ca2993e528fe7ce1eea2c6d0df8d740be5ef8e0
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-19 22:46:13 +0530

    Added folder 29

---
+++ 29_wrapper_classes/D.java
@@ -0,0 +1,15 @@
+class D {
+
+     public static void main(String[] args) {
+
+         Double a = new Double(3.4);
+
+         System.out.println(a);
+
+     }
+}
+
+//  D.java:5: warning: [removal] Double(double) in Double has been deprecated and marked for removal
+//          Double a = new Double(3.4);
+//                     ^
+// 1 warning
