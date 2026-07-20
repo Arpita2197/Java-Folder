f2db31cf920ea63015507eec8031376b50ffaeac
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-20 09:32:30 +0530

    Added folder 29

---
+++ 29_wrapper_classes/I.java
@@ -0,0 +1,14 @@
+class I {
+
+    
+    public static void main(String[] args) { 
+
+        Integer  a = 15;               // AUTO - BOXING......
+
+        System.out.println(a);
+
+    }
+
+}
+
+     
