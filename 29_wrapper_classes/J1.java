ba49e02cf884c4a3f076453d57de952cde32770e
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-20 09:33:12 +0530

    Added folder 29

---
+++ 29_wrapper_classes/J1.java
@@ -0,0 +1,17 @@
+class J1 {
+   
+    public static void main(String[] args) { 
+
+        Float  a = 15.29;               // AUTO - BOXING......
+
+        System.out.println(a);
+
+    }
+
+}
+
+//  C:\Users\lenovo\Desktop\java\29_wrapper_classes>javac J1.java
+// J1.java:6: error: incompatible types: double cannot be converted to Float
+//         Float  a = 15.29;               // AUTO - BOXING......
+//                    ^
+// 1 error
