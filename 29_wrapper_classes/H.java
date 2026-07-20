d6a8f0399955e1d3c42e1bb033a603d5c7031323
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-20 09:32:19 +0530

    Added folder 29

---
+++ 29_wrapper_classes/H.java
@@ -0,0 +1,16 @@
+class H {
+
+    
+    public static void main(String[] args) { 
+
+        Boolean a = true;               // AUTO - BOXING......
+
+        Boolean b = false;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+
+}
