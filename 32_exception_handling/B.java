20115df93be78175071c8377357a423d19f728da
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-22 21:54:09 +0530

    Added folder 32

---
+++ 32_exception_handling/B.java
@@ -0,0 +1,15 @@
+class B {
+
+     public static void main (String[] args) {
+
+         finally {
+
+             
+         }
+     }
+}
+
+//  B.java:5: error: 'finally' without 'try'
+//          finally {
+//          ^
+// 1 error
