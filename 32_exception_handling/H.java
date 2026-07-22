36df3ed379829fc3ca0715a2f57f88e520a46cf7
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-22 21:56:18 +0530

    Added folder 32

---
+++ 32_exception_handling/H.java
@@ -0,0 +1,22 @@
+class H {
+
+     public static void main(String[] args) {
+
+          System.out.println("A");
+
+          try {
+                String a = args[0];
+          } catch(ArithmeticException e) {
+
+             System.out.println(e);
+          }
+
+              System.out.println("B");
+          
+     }
+}
+
+ //  for input ==> <blank>
+//  A
+// Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 0 out of bounds for length 0
+//         at H.main(H.java:10)
