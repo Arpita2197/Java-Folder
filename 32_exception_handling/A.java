9230938a9ef6bdb82f20e96d62a4286ef1f33b24
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-22 21:53:52 +0530

    Added folder 32

---
+++ 32_exception_handling/A.java
@@ -0,0 +1,15 @@
+class A {
+
+     public static void main (String[] args) {
+
+         catch(Exception e) {
+
+             
+         }
+     }
+}
+
+//  A.java:5: error: 'catch' without 'try'
+//          catch(Exception e) {
+//          ^
+// 1 error
