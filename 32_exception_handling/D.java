61c82751efe88a72f2de554bae3726b96c089365
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-22 21:54:26 +0530

    Added folder 32

---
+++ 32_exception_handling/D.java
@@ -0,0 +1,38 @@
+class D {
+
+     public static void main(String[] args) {
+
+         try {
+
+             try {
+
+             }
+
+             catch(ArithmeticException e) {
+
+             }finally{
+
+             }
+         } catch(ArrayIndexOutOfBoundsException e) {
+             
+             try {
+
+             }catch(NumberFormatException f ) {
+
+
+             }finally{
+
+                 
+             }
+         }finally {
+
+            try {
+
+            } catch(StringIndexOutOfBoundsException e) {
+
+            }finally{
+
+            }
+         }
+     }
+}
