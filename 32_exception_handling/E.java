8b13b9aba2f99de960a1d0ad3c05556d4556fcd7
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-22 21:54:55 +0530

    Added folder 32

---
+++ 32_exception_handling/E.java
@@ -0,0 +1,35 @@
+class E {
+
+     public static void main(String[] args) {
+
+          // some code....
+
+          try {
+
+          }catch(Exception e) {
+
+
+          }
+
+            // some code....
+             // some code....
+              // some code....
+
+            try {
+
+            }  catch(ArithmeticException e) {
+
+            }
+
+             // some code....
+             // some code....
+              // some code....
+
+              try {
+
+              } catch(NumberFormatException e) {
+                
+              }
+         
+     }
+}
