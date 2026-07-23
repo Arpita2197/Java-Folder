69c5256699aff36d25ff5400267cdecd2d4b83a3
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-23 07:28:02 +0530

    Added folder 32

---
+++ 32_exception_handling/L.java
@@ -0,0 +1,13 @@
+class L /* extends RuntimeException */  {
+
+    public static void main(String[] args) {
+         
+        try{
+
+
+        } catch(L x ) {
+
+             
+        }
+    }
+}
