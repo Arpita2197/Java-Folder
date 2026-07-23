223e9c8b2340becc29011c7c1527497b370525e4
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-23 07:26:44 +0530

    Added folder 32

---
+++ 32_exception_handling/J2.java
@@ -0,0 +1,13 @@
+class J2 {
+
+    public static void main(String[] args) {
+
+         try {
+
+
+         } catch(Exception e ) {    // Exception = checked + unchecked dono hai....
+
+
+         }
+    }
+}
