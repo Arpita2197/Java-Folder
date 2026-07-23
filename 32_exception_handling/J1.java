537ca51712a4f98f4226317d3c78952197e8cec1
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-23 07:26:26 +0530

    Added folder 32

---
+++ 32_exception_handling/J1.java
@@ -0,0 +1,19 @@
+import java.io.FileNotFoundException;
+
+class J1 {
+
+     public static void main(String[] args) {
+
+         try {
+                 // checked exception.....
+                 
+             FileNotFoundException x = new FileNotFoundException();
+
+             throw x;
+
+         } catch(Exception e ) {
+
+
+         }
+     }
+}
