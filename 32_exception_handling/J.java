9f716bfcfb87dae3d6221e73757197b2a64f4617
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-23 07:26:05 +0530

    Added folder 32

---
+++ 32_exception_handling/J.java
@@ -0,0 +1,18 @@
+class J {
+
+     public static void main(String[] args) {
+
+        try {
+
+               // unchecked exception...
+               
+            ArithmeticException x = new ArithmeticException();
+
+            throw x;
+
+        } catch(Exception e) {
+
+
+        }
+     }
+}
