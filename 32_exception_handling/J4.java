a85a7b6f74b317bc327539e77de3f6de5f6e9047
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-23 07:27:24 +0530

    Added folder 32

---
+++ 32_exception_handling/J4.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+class J4 {
+
+    public static void main(String[] args) {
+
+         try {
+
+         }catch(IOException e) {          // checked Exception...
+
+
+         }
+    }
+}
+
+//  J4.java:9: error: exception IOException is never thrown in body of corresponding try statement
+//          }catch(IOException e) {     // IOException is a child of Exception
+//           ^
+// 1 error
