22bb221416ab533185474bb4de21a5e525b1d079
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-25 10:21:50 +0530

    Added folder 32

---
+++ 32_exception_handling/U.java
@@ -0,0 +1,32 @@
+class U {
+
+    public static void main(String[] args) {
+
+         System.out.println("A");
+
+         try {
+
+             System.out.println("X");
+
+             int y = 12 / 0;
+
+                System.out.println("Y");
+
+         } catch(ArithmeticException e) {
+
+            // e.printStackTrace();
+
+             throw new NullPointerException();
+
+         } finally {
+
+                System.out.println("Z");
+
+                if(12 < 13) {
+                     throw new ClassCastException();      // exception yeah wali print hogi........
+                }
+         }
+
+             System.out.println("B");
+    }
+}
