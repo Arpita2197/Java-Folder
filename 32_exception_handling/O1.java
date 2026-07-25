a6111fce8a98cd0bd741a787a4caedf705fdab69
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-25 10:19:53 +0530

    Added folder 32

---
+++ 32_exception_handling/O1.java
@@ -0,0 +1,30 @@
+class O1 {
+
+     public static void main(String[] args) {
+
+        System.out.println("A");
+
+        if (args.length>0) {
+
+            System.out.println(args[0]);
+        }
+            else {
+               
+                try {
+                       throw  new NoCommandLineArgumentException();
+                       
+                } catch( NoCommandLineArgumentException e) {
+
+                     System.out.println(e);
+                }
+              
+            }
+
+             System.out.println("B");
+        
+     }
+}
+
+class NoCommandLineArgumentException extends RuntimeException {
+
+}
