52a28451cab590b091c9b4c5dd270411a1b8db14
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-25 10:20:50 +0530

    Added folder 32

---
+++ 32_exception_handling/R.java
@@ -0,0 +1,19 @@
+class R {
+
+    public static void main(String[] args) {
+
+         try {
+
+            throw new ArithmeticException ("ghapla ho gaya");
+
+         } catch(ArithmeticException e) {
+
+         //    System.out.println(e);
+
+            System.out.println(e.getMessage());
+         }
+    }
+}
+
+
+//  output ==> ghapla ho gaya
