8946049bd9b7bf76c9d5f82208bebb18159820e8
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-27 16:38:31 +0530

    Added folder 33

---
+++ 33_exception_handling/M.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.EOFException;
+
+class E1 {
+    
+    void pro() throws FileNotFoundException {
+
+    }
+}
+
+class E2 extends E1 {
+
+    // ok: throws fewer exception... hint: ClassCastException is an Unchecked Exception
+    void pro() throws ClassCastException  {
+
+    }
+}
