2408be3e9ce12ddda961d0057fbfd5ba61508e8c
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-28 20:21:11 +0530

    Added folder 34

---
+++ 34_stack_trace/I.java
@@ -0,0 +1,21 @@
+class I {
+
+    static {
+
+        System.out.println("A");
+
+         int y = 12 / 0;
+
+         System.out.println("B");
+    }
+
+     public static void main(String[] args) {
+
+         System.out.println("C");
+     }
+}
+
+//  A
+// Exception in thread "main" java.lang.ExceptionInInitializerError
+// Caused by: java.lang.ArithmeticException: / by zero
+//         at I.<clinit>(I.java:7)
