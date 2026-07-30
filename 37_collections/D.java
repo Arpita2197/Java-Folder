85068643157e58a8406ff16fd3665043dc1be5e5
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-30 22:04:11 +0530

    Added folder 37

---
+++ 37_collections/D.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+class D {
+
+     public static void main(String[] args) {
+
+         
+          ArrayList x = new ArrayList();
+
+          System.out.println(x);
+          System.out.println(x.isEmpty());
+          System.out.println(x.size());
+
+           x.add("mohan");
+           x.add("sohan");
+           x.add("geet");
+            
+             System.out.println("~~~~~~~~");
+
+            System.out.println(x);
+          System.out.println(x.isEmpty());
+          System.out.println(x.size());
+
+
+
+     }
+
+}     
