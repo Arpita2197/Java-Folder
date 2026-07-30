e29b2e636f6405e180be3a8f550706a64674be81
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-30 22:03:18 +0530

    Added folder 37

---
+++ 37_collections/A.java
@@ -0,0 +1,19 @@
+class A {
+
+     public static void main(String[] args) {
+
+         ArrayList x = new ArrayList();
+     }
+}
+
+//  A.java:5: error: cannot find symbol
+//          ArrayList x = new ArrayList();
+//          ^
+//   symbol:   class ArrayList
+//   location: class A
+// A.java:5: error: cannot find symbol
+//          ArrayList x = new ArrayList();
+//                            ^
+//   symbol:   class ArrayList
+//   location: class A
+// 2 errors
