f3eea5600351ebad29e7f0b578c9a52dc2453380
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-30 22:03:49 +0530

    Added folder 37

---
+++ 37_collections/B.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+class B {
+
+     public static void main(String[] args) {
+
+         ArrayList x = new ArrayList();
+
+
+           System.out.println(x);
+          System.out.println(x.isEmpty());
+          System.out.println(x.size());
+
+
+
+
+     }
+}
