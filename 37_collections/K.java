6450bb4aff5fde668e01e5e4bc05aa529c2f37b9
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-30 22:05:35 +0530

    Added folder 37

---
+++ 37_collections/K.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class K {
+
+    public static void main(String[] args) {
+        ArrayList x = new ArrayList();
+
+        System.out.println(x);
+
+        x.add("mohan");
+        x.add("dinesh");
+        x.add("vidit");
+        x.add("sarvesh");
+        x.add("prayag");
+
+        System.out.println(x);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+
+        Iterator y = x.iterator();
+
+        System.out.println("iterator subclass :"  + y );
+
+    }
+
+
+}    
