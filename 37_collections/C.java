679e66163427d1aa921f98231c6ae418e8317cd6
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-30 22:03:57 +0530

    Added folder 37

---
+++ 37_collections/C.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+class C {
+
+     public static void main(String[] args) {
+
+         
+          ArrayList x = new ArrayList();
+
+          System.out.println(x);
+          System.out.println(x.isEmpty());
+          System.out.println(x.size());
+
+
+        x.add(10);  
+        x.add(15);
+        x.add(100);
+        x.add(104);
+        
+         
+         System.out.println("~~~~~~~~~~~~~~~~");
+
+          System.out.println(x);
+          System.out.println(x.isEmpty());
+          System.out.println(x.size());
+
+     }
+}
