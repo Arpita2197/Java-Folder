645bb011f6cbb2bfd23521c5b559e9872f3505fe
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-31 20:41:47 +0530

    Added folder 37

---
+++ 37_collections/R2.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class R2 {
+
+     public static void main(String[] args) {
+         
+         
+         ArrayList <Integer> x = new ArrayList <Integer>();
+
+        x.add(10);  
+        x.add(15);
+        x.add(100);
+        x.add(104);
+
+        
+         System.out.println(x);
+         
+         x.add(1001);
+
+         System.out.println(x);
+         
+     }
+}
