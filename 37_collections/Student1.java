24d4ab85c1f5038e94b13283faa746218e2431ec
Arpita2197 <arpitasahu2197@gmail.com>
2026-07-31 20:44:14 +0530

    Added folder 37

---
+++ 37_collections/Student1.java
@@ -0,0 +1,16 @@
+class Student1 {
+     
+     String name;
+     int age;
+
+     Student1(String name , int age) {
+
+         this.name=name;
+         this.age=age;
+     }
+
+      public String toString() {        // alpha class Object ka method ha
+
+          return name + "-" + age;
+      }
+}
