041ac1505a01b7fbce2b7558661864fdf922b942
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-01 10:09:37 +0530

    Added folder 37

---
+++ 37_collections/V1.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+class V1 {
+
+     public static void main(String[] args) {
+
+         ArrayList <Student1> x = new ArrayList <Student1> ();
+
+         Student1 a = new Student1("jay" , 23);
+         Student1 b = new Student1("raghav" , 18);
+         Student1 c = new Student1("golu" , 9);
+         Student1 d = new Student1("vikram" , 12);
+         Student1 e = new Student1("om" , 17);
+
+           x.add(a);
+           x.add(b);
+           x.add(c); 
+           x.add(d);
+           x.add(e);
+
+            System.out.println(x);
+
+
+     }
+}
