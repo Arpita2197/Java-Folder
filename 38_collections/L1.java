1364c00e7cbd7214c3ae2f6119e26fe5309c67cc
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-02 10:27:43 +0530

    Added folder 38

---
+++ 38_collections/L1.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+ class L1 {
+
+      public static void main(String[] args) {
+
+           HashSet <Student> x = new HashSet <Student> ();
+
+            x.add (new Student("ram" , 18));
+            x.add (new Student("raju" , 19));
+            x.add (new Student("raghu" , 20));
+            x.add (new Student("raghav" , 21));
+            x.add (new Student("rahu" , 22));
+
+           System.out.println(x);
+      
+          Student y = new Student("raghav" , 21);         // ref code differ
+
+           System.out.println(x.remove(y));      // false
+                   
+
+      }
+ }
