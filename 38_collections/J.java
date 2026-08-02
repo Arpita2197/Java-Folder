db5be4800df22383b3a7ab1650328ffd5d008ac4
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-02 10:27:20 +0530

    Added folder 38

---
+++ 38_collections/J.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+class J {
+    
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<String>();
+
+        set.add("kamal");
+        set.add("saroj");
+        set.add("vineet");
+        set.add("rahul");
+        set.add("gagan");
+        
+        System.out.println(set);
+
+        for(String next : set) {
+            System.out.println(next);
+        }        
+    }
+}
