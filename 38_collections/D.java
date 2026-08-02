09f71c2e521964dc9f186cb743d45f6cc22ae6dc
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-02 10:26:32 +0530

    Added folder 38

---
+++ 38_collections/D.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+class D {
+
+
+     public static void main(String[] args) {
+
+         HashSet <String> x = new HashSet<String> ();
+
+          x.add("kamal");
+          x.add("mukesh");
+          x.add("raju");
+          x.add("sonu");
+
+          System.out.println(x);       // neither ordered nor sorted
+
+                                      // not a index based
+          
+     }
+}
