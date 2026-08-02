64b6c9c0bb2433e21ba52ceb7bf1e6456e1b8d47
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-02 10:26:40 +0530

    Added folder 38

---
+++ 38_collections/E.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+class E {
+
+
+     public static void main(String[] args) {
+
+         HashSet <String> x = new HashSet<String> ();
+          
+          System.out.println(x.isEmpty());   
+
+            System.out.println(x.size());       
+
+          x.add("kamal");
+          x.add("mukesh");
+          x.add("raju");
+          x.add("sonu");
+
+          System.out.println(x);   
+
+           System.out.println(x.isEmpty());  
+            
+            System.out.println(x.size());       
+
+                                      
+          
+     }
+}
