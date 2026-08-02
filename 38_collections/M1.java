e31e870992583b2984fd845eb8528d5c8fbb7b77
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-02 10:28:05 +0530

    Added folder 38

---
+++ 38_collections/M1.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+ class M1 {
+
+      public static void main(String[] args) {
+
+           HashSet <Student1> x = new HashSet <Student1> ();
+
+            x.add (new Student1("ram" , 18));
+            x.add (new Student1("raju" , 19));
+            x.add (new Student1("raghu" , 20));
+            x.add (new Student1("raghav" , 21));
+            x.add (new Student1("rahu" , 22));
+
+           System.out.println(x);
+      
+          Student1 y = new Student1("raghav" , 21);        
+
+           System.out.println(x.remove(y));     
+                   
+
+      }
+ }
