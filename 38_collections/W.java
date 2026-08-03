7231322b0628ea2c67934d0762b941ad5bcc5942
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-03 21:18:28 +0530

    Added folder 38

---
+++ 38_collections/W.java
@@ -0,0 +1,18 @@
+import java.util.TreeSet;
+
+class W {
+
+     public static void main(String[] args) {
+
+         TreeSet <Integer> x = new TreeSet <Integer> ();
+
+          x.add(18);
+          x.add(20);
+          x.add(44);
+          x.add(60);
+          x.add(56);
+
+           System.out.println(x);      // ordered and sorted
+
+     }
+}
