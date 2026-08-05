f0fcee5dfec22f1b80681a19746fb42dfa38d16b
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-05 10:00:35 +0530

    Added folder 39

---
+++ 39_Collections/D.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;          // HashMap < K , V >         (K = key , V = value)
+
+ class D {
+
+      public static void main(String[] args) {
+
+     HashMap x = new HashMap();
+
+      System.out.println(x.isEmpty());
+      System.out.println(x.size());
+   }
+
+ }
+ 
