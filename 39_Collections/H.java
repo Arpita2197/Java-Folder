27e2cae8ab829001f017b5d69c54a02251b03412
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-05 10:01:07 +0530

    Added folder 39

---
+++ 39_Collections/H.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class H {
+
+    public static void main(String[] args) {
+        HashMap <String , Double>  x = new HashMap <String , Double> ();
+
+        System.out.println(x);
+        
+        x.put("Mohan", 78.89);
+        x.put("Rohan", 88.12);
+        x.put("Sohan", 63.54);
+        x.put("Ganesh", 99.99);
+        x.put("Mangesh", 82.59);
+
+          String str = new String("Sohan");
+
+         // String str = new String("Indra");
+        
+        System.out.println(x.containsKey(str));
+
+         
+    }
+}
