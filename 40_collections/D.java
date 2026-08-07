d4d8951a927cf89023c6ba4ba7b88a90495006c6
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-07 18:28:31 +0530

    Added folder 40

---
+++ 40_collections/D.java
@@ -0,0 +1,19 @@
+import java.util.PriorityQueue;
+
+class D {
+
+     public static void main (String[] args) {
+
+         PriorityQueue <String> x = new PriorityQueue <String> ();
+
+         x.offer("mohan");        
+         x.offer("gohan");        
+         x.offer(" ram ");        
+         x.offer(" lara ");   
+         x.offer("golu");   
+         x.offer(" kajju "); 
+         x.offer("Zebra ");
+
+          System.out.println(x);
+     }
+}
