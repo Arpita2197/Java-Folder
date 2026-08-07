c7ae9185c9e279ecc2add6d423502c102c88a2f6
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-07 18:28:58 +0530

    Added folder 40

---
+++ 40_collections/G.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+
+class G {
+
+     public static void main (String[] args) {
+
+         PriorityQueue <Integer> x = new PriorityQueue <Integer> ();
+
+         x.offer(88);        
+         x.offer(11);        
+         x.offer(56);        
+         x.offer(65);   
+         x.offer(100);   
+         x.offer(45); 
+         x.offer(99);
+          
+         System.out.println(x.poll());         
+         System.out.println(x.poll()); 
+         System.out.println(x.poll());         
+         System.out.println(x.poll());  
+         System.out.println(x.poll());   
+         System.out.println(x.poll()); 
+         System.out.println(x.poll());
+
+     }
+}
