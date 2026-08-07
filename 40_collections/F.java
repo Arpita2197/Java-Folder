cf7290cc1322010cc31600a7a6f2e5a3a9314f76
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-07 18:28:49 +0530

    Added folder 40

---
+++ 40_collections/F.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+
+class F {
+
+     public static void main (String[] args) {
+
+         PriorityQueue <Integer> x = new PriorityQueue <Integer> ();
+
+         x.offer(88);        
+         x.offer(11);        
+         x.offer(56);        
+         x.offer(65);   
+         x.offer(100);   
+         x.offer(45); 
+         x.offer(99);
+          
+
+        System.out.println(x);
+     }
+}
