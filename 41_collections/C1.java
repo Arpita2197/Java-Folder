a9c24683260c7d0b57248dda22f64d43c8592d14
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-09 19:54:55 +0530

    Added folder 41

---
+++ 41_collections/C1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+class C1 {
+
+    public static void main(String[] args) {
+
+        int[] x = {12,56,9};
+
+        List  list = Arrays.asList(x);
+
+              System.out.println(list);
+
+    }
+}
