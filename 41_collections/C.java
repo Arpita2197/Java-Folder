704d52dbef9bf40f9656aa361762ef39063c9c5b
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-09 19:54:36 +0530

    Added folder 41

---
+++ 41_collections/C.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+class C {
+
+    public static void main(String[] args) {
+
+        int[] x = {12,56,9};
+
+        List <int[]> list = Arrays.asList(x);
+
+        for(Object next : x) {
+
+             System.out.println(next);
+        }
+
+              System.out.println(list);
+
+    }
+}
