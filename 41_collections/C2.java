1b0b8b9cc051d43f644c3597b23b911b9170461e
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-09 19:55:04 +0530

    Added folder 41

---
+++ 41_collections/C2.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+class C2 {
+
+    public static void main(String[] args) {
+
+        int[] x = {12,56,9};
+
+        List <int[]> list = Arrays.asList(x);
+
+        for ( int[] next : list) {
+
+             for(int nx : next)
+
+             System.out.println(nx);
+        }         
+
+    }
+}
