ba143c7c3b4f61eba24a657c4201258616e1db45
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-10 22:19:24 +0530

    Added folder 42

---
+++ 42_generics/E.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+class E {
+
+    public static void main(String[] args) {
+
+         ArrayList <Integer> a = new ArrayList <Integer>();       
+         ArrayList <Runnable> b  = new ArrayList <Runnable>();
+         ArrayList <Student> c  = new ArrayList <Student>();
+         ArrayList <Days> d  = new ArrayList <Days>(); 
+         ArrayList <User> e  = new ArrayList <User>();
+         ArrayList <XYZ.UVW> f  = new ArrayList <XYZ.UVW>();
+          ArrayList <int[]> g  = new ArrayList <int[]>();
+
+         ArrayList <String[]> h  = new ArrayList <String[]>();
+        ArrayList <Object> i  = new ArrayList <Object>();
+
+          
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+        System.out.println(h);
+       System.out.println(i);
+       
+    }
+}
