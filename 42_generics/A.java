9f334b5530d9ce7f506e74c5f302409cef04166d
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-10 22:18:07 +0530

    Added folder 42

---
+++ 42_generics/A.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class A {
+
+    public static void main(String[] args) {
+
+        ArrayList x = new ArrayList();
+
+        // x.add(23);
+        // x.add(true);
+        // x.add("om");
+        // x.add(15.29);
+
+        System.out.println(x);
+    }
+
+}
