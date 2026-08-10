eda0aa0d76381c148eb2a3b45d1744829069ba54
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-10 22:19:05 +0530

    Added folder 42

---
+++ 42_generics/D1.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class D1 {
+
+    public static void main(String[] args) {
+
+        ArrayList x = new ArrayList  ();
+
+        x.add(23);
+        x.add(15);
+        x.add(29);
+        x.add(30);
+
+        System.out.println(x);
+
+        Integer y = (Integer)x.get(0);
+
+       
+    }
+
+}
