2d8d988daed54388943e045de88757562c7fa611
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-11 10:21:42 +0530

    Added folder 42

---
+++ 42_generics/I1.java
@@ -0,0 +1,14 @@
+class I1 {
+
+    public static void main(String[] args) {
+
+        int[] x = new int[2];
+
+        int[] y = new byte[2];
+    }
+}
+
+//  I.java:7: error: incompatible types: byte[] cannot be converted to int[]
+//         int[] y = new byte[2];
+//                   ^
+// 1 error
