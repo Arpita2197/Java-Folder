5b18e9528fe7c9bc829156cb53ad438ae7fb3d43
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-11 10:21:52 +0530

    Added folder 42

---
+++ 42_generics/J.java
@@ -0,0 +1,15 @@
+class J {
+
+    public static void main(String[] args) {
+
+        Animal x = new Cat();
+
+        Animal[] y = new Cat[2];
+
+        
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+}
