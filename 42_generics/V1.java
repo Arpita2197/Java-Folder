91485fe86683a2e3d36a6a192bb2f65565193f3a
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-12 21:46:57 +0530

    Added folder 42

---
+++ 42_generics/V1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class V1 {
+
+    public static void main(String[] args) {
+
+         Object x = new ArrayList <Integer> ();
+
+         ArrayList <Integer> list = (ArrayList <Integer>) x;
+
+         System.out.println(list);
+    }
+}
