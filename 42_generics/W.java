a4d513bfbdc5e1f0cdb09189c74146f2917eabc4
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-12 21:47:21 +0530

    Added folder 42

---
+++ 42_generics/W.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+class W {
+
+    public static void main(String[] args) {
+
+        ArrayList <Integer> a = new ArrayList<>();
+        ArrayList <Float> b = new ArrayList <>();
+
+        pro(a);
+        pro(b);
+
+    }
+
+     static void pro(ArrayList<Number> list) {
+
+         // problem no.1...
+
+         for(Object next : list) {
+
+
+         }
+
+          // problem no. 2 ....
+
+           list.add(25);
+           list.add(15.29);
+     }
+}
+
+//  W.java:10: error: incompatible types: ArrayList<Integer> cannot be converted to ArrayList<Number>
+//         pro(a);
+//             ^
+// W.java:11: error: incompatible types: ArrayList<Float> cannot be converted to ArrayList<Number>
+//         pro(b);
+//             ^
+// Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output
+// 2 errors
