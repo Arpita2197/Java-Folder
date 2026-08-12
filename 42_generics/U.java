d66049509f3737af23b24e5a223198f810f168e0
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-12 21:46:39 +0530

    Added folder 42

---
+++ 42_generics/U.java
@@ -0,0 +1,19 @@
+class Teacher implements Comparable <Teacher> {
+   
+    public int compareTo(Teacher t ) {
+
+        //------
+
+        return 1;               // type-safety kha - kha use kar sakta hai.....
+
+    }
+     
+}
+
+class U {
+
+    public static void main(String[] args) {
+
+
+    }
+}
