10c23454261afe13bb0cfc941de7af9d8860f124
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-13 22:43:57 +0530

    Added folder 43

---
+++ 43_generics/C1.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.HashSet;
+
+
+class C1 {
+
+    public static void main(String[] args) {
+
+       HashSet<List> x1 = new HashSet<ArrayList>();
+       HashSet<List> x2 = new HashSet<Vector>();
+       HashSet<List> x3 = new HashSet<LinkedList>();
+
+    
+    }
+}
+
+// C1.java:12: error: incompatible types: HashSet<ArrayList> cannot be converted to HashSet<List>
+//        HashSet<List> x1 = new HashSet<ArrayList>();
+//                           ^
+// C1.java:13: error: incompatible types: HashSet<Vector> cannot be converted to HashSet<List>
+//        HashSet<List> x2 = new HashSet<Vector>();
+//                           ^
+// C1.java:14: error: incompatible types: HashSet<LinkedList> cannot be converted to HashSet<List>
+//        HashSet<List> x3 = new HashSet<LinkedList>();
+//                           ^
+// 3 errors
