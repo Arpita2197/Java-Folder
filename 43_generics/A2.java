89f5a86abbb7d01d7f9f363375e201833936d754
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-13 22:42:33 +0530

    Added folder 43

---
+++ 43_generics/A2.java
@@ -0,0 +1,22 @@
+// import java.util.ArrayList;
+// import java.util.HashSet;
+// import java.util.Date;
+
+import java.util.*;
+
+class A2 {
+
+    public static void main(String[] args) {
+
+        ArrayList <HashSet> x = new ArrayList<> ();
+        
+
+        x.add(new HashSet<Integer>());
+        x.add(new HashSet<String>());
+        x.add(new HashSet<Date>());
+
+
+        System.out.println(x);
+
+    }
+}
