42676766eec65a18288493b948af816bc7e9c456
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-14 22:34:27 +0530

    Added folder 43

---
+++ 44_fileHandling/A.java
@@ -0,0 +1,21 @@
+class A {
+
+    public static void main(String[] args) {
+
+         File file = new File();
+
+
+    }
+} 
+
+// A.java:5: error: cannot find symbol
+//          File file = new File();
+//          ^
+//   symbol:   class File
+//   location: class A
+// A.java:5: error: cannot find symbol
+//          File file = new File();
+//                          ^
+//   symbol:   class File
+//   location: class A
+// 2 errors
