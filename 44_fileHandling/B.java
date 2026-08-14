9f3de844b8c5c355e4d7e89f3a1ef40bb1d0e962
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-14 22:35:03 +0530

    Added folder 44

---
+++ 44_fileHandling/B.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+class B {
+
+
+    public static void main (String[] args) {
+
+        File file = new File("abc.txt");           // File class ka sara constructors parameterized ha.........
+
+        boolean flag = file.exists();             // yeah current directory ma check karaga it means 44_fileHandling folder ma
+
+        System.out.println("abc.txt exists :" + flag);
+    }
+}
+
+
+ // o/p == false
