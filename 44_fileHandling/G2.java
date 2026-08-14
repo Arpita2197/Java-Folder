db77bcd63c8b52c93d664b4839d9fe342cc3acee
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-14 22:37:10 +0530

    Added folder 44

---
+++ 44_fileHandling/G2.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+class G2 {
+
+
+    public static void main (String[] args) {
+ 
+        File file = new File("C:\\ Users\\ lenovo \\ OneDrive \\ Desktop \\ 44_fileHandling \\ xyx.java");          // backward slash       
+
+        boolean flag = file.exists();        
+
+        System.out.println("C:\\ Users \\ lenovo \\ OneDrive \\ Desktop \\ 44_fileHandling \\ xyx.java :" + flag) ;
+    
+}
+
+}
+
