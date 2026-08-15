6f85e74a0a6df53181b80d1a408b1443a6524cfc
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-15 22:36:00 +0530

    Added folder 48

---
+++ 48_pattern_matcher/B.java
@@ -0,0 +1,15 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+class B {
+
+    public static void main (String[] args) {
+
+         Pattern p = new Pattern(" " , 0);
+    }
+}
+
+//  B.java:8: error: Pattern(String,int) has private access in Pattern
+//          Pattern p = new Pattern(" " , 0);
+//                      ^
+// 1 error
