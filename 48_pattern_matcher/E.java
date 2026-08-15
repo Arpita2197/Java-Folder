d67d88673940556bfbb5e672a995c6356e093255
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-15 22:36:29 +0530

    Added folder 48

---
+++ 48_pattern_matcher/E.java
@@ -0,0 +1,15 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+class E {
+
+    public static void main(String[] args) {
+
+         Pattern p = Pattern.compile("gohan");
+
+         System.out.println(p);
+
+         System.out.println(p.toString());
+
+    }
+}   
