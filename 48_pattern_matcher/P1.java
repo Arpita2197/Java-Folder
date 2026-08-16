1269968e59331ea254a42da8e320379d445c2375
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-16 10:34:48 +0530

    Added folder 48

---
+++ 48_pattern_matcher/P1.java
@@ -0,0 +1,19 @@
+                
+  import java.util.regex.Pattern;
+  import java.util.regex.Matcher;
+
+  class P1 {
+
+     public static void main(String[] args) {
+
+     
+         Pattern p = Pattern.compile("[pqr]");                    
+         Matcher m = p.matcher("cyegrtRbdPaTrRQqbp");
+
+          while(m.find()) {
+
+             System.out.println(m.start() + " - " + m.end() + " - " + m.group());
+
+          }
+     }
+  }   
