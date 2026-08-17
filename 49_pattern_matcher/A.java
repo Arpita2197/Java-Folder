3b42ad4d4d9d2e31b530a15274d84fd79347abe4
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-17 18:15:29 +0530

    Added folder 49

---
+++ 49_pattern_matcher/A.java
@@ -0,0 +1,19 @@
+                
+  import java.util.regex.Pattern;
+  import java.util.regex.Matcher;
+
+  class A {
+
+     public static void main(String[] args) {
+
+     
+         Pattern p = Pattern.compile(".+?xx");           
+         Matcher m = p.matcher("_a7kRxx9i 12exx50 &^Eq2");
+
+          while(m.find()) {
+
+             System.out.println(m.start() + " - " + m.end() + " - " + m.group());
+
+          }
+     }
+  } 
