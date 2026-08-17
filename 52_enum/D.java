5d2da3436d2e106e0e234904d02764e335531f41
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-17 18:45:03 +0530

    Added folder 52

---
+++ 52_enum/D.java
@@ -0,0 +1,15 @@
+enum Days {
+
+    w2,2t,for,x~y      // identifiers ka rule ko follow karo...
+}
+
+//  D.java:3: error: enum constant expected here
+//     w2,2t,for,x~y
+//        ^
+// D.java:3: error: illegal start of type
+//     w2,2t,for,x~y
+//           ^
+// D.java:3: error: ',', '}', or ';' expected
+//     w2,2t,for,x~y
+//                ^
+// 3 errors
