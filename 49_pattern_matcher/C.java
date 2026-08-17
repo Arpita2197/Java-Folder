d54d6357ac284c587f10a228dbcc368e5c737377
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-17 18:15:47 +0530

    Added folder 49

---
+++ 49_pattern_matcher/C.java
@@ -0,0 +1,16 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+class C {
+
+    public static void main(String[] args) {
+
+        Pattern p = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
+
+       Matcher m = p.matcher("my email: hitesh_234.32@gmail.com and your email: golu_23@altavista.com and his email is: raju007@tcs");
+
+        
+        System.out.println(m.find());
+        System.out.println(m.matches());  // matches() ==> means entire string email ha ha ki nhe....
+    }    
+}
