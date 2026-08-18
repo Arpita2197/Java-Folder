16b4e631545dd223534cd8eb7250be1c436dea67
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-18 21:07:43 +0530

    Added folder 52

---
+++ 52_enum/H.java
@@ -0,0 +1,15 @@
+enum Days {      // Days as a data - type
+
+    SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY      // static final constants...
+}
+
+ class H {
+
+    static Days x = Days.SUNDAY;  // ham data type Days ma value initialize kar raha ha..
+
+      public static void main(String[] args) {
+
+         System.out.println(x);
+
+      }
+ }
