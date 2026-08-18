155fb898d7e73fff407aee9a27860c6c509080f4
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-18 21:09:14 +0530

    Added folder 52

---
+++ 52_enum/O.java
@@ -0,0 +1,12 @@
+private enum CoffeMugSize {
+
+    SMALL,MEDIUM,LARGE;
+}
+
+//  C:\Users\lenovo\OneDrive\Desktop\java\52_enum>javac O.java
+// O.java:1: error: modifier private not allowed here
+// private enum CoffeMugSize {
+//         ^
+// 1 error
+
+
