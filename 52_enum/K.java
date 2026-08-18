5e737eccd85871caa6281711768f58f4d1a92784
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-18 21:08:39 +0530

    Added folder 52

---
+++ 52_enum/K.java
@@ -0,0 +1,18 @@
+class XX {
+
+    enum Days {
+
+     SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY
+ }
+
+}
+
+class K {
+
+    public static void main(String[] args) {
+
+         XX.Days x = XX.Days.MONDAY;
+
+         System.out.println(x);
+    }
+}
