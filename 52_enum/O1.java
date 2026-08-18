3fb82350847ccb72aceb062b7efae6bc54c926e4
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-18 21:09:29 +0530

    Added folder 52

---
+++ 52_enum/O1.java
@@ -0,0 +1,11 @@
+protected enum CoffeMugSize {
+
+    SMALL,MEDIUM,LARGE;
+}
+
+//  C:\Users\lenovo\OneDrive\Desktop\java\52_enum>javac O1.java
+// O1.java:1: error: modifier protected not allowed here
+// protected enum CoffeMugSize {
+//           ^
+// 1 error
+
