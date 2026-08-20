bf2272e98642748585c89afb81a1e97b063cc23c
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-20 10:16:03 +0530

    Added folder 52

---
+++ 52_enum/V.java
@@ -0,0 +1,20 @@
+enum AAA {
+
+
+}
+
+enum V extends AAA {
+
+
+}
+
+//  V.java:6: error: '{' expected
+// enum V extends AAA {
+//       ^
+// V.java:6: error: enum constant expected here
+// enum V extends AAA {
+//        ^
+// V.java:9: error: reached end of file while parsing
+// }
+//  ^
+// 3 errors
