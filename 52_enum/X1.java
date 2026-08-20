36682256d1c01385444a9762ec5e3a650065c11d
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-20 10:17:03 +0530

    Added folder 52

---
+++ 52_enum/X1.java
@@ -0,0 +1,19 @@
+enum CC {
+
+     AA,BB;
+
+     class Z {
+
+
+     }
+
+      static class Y {
+
+         
+      }
+}
+
+ // enum ko yeah toh khali choodo,
+ // yeah....
+ // enum ma kuch karna ha toh phela jis cheez ka liya enum design hua ha (constants banana ka liya) usko phela banana hee padaga
+ // phir jo - jo banana ha usma bananano...
