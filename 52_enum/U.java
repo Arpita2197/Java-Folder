7272ad4720de3640a4ee33bec7794ae6b5be0058
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-20 10:15:31 +0530

    Added folder 52

---
+++ 52_enum/U.java
@@ -0,0 +1,26 @@
+class U {
+
+}
+
+enum Days extends Days{
+
+     SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY
+}
+
+
+//  U.java:5: error: '{' expected
+// enum Days extends Days{
+//          ^
+// U.java:5: error: enum constant expected here
+// enum Days extends Days{
+//           ^
+// U.java:7: error: <identifier> expected
+//      SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY
+//            ^
+// U.java:7: error: ';' expected
+//      SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY
+//                                                              ^
+// U.java:8: error: reached end of file while parsing
+// }
+//  ^
+// 5 errors
