b8de8458ed0bf53537b2a216e91a98643fd16750
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-20 10:16:50 +0530

    Added folder 52

---
+++ 52_enum/Y.java
@@ -0,0 +1,23 @@
+enum Y {
+                // enum ma sabsa phela priority uska constants ko milti haii....
+    A,B,C;        // static...constants
+
+    {
+      
+       System.out.println("HI");
+
+    }
+
+     static  {            // static block bss ek baar load hota hai....
+      
+       System.out.println("Hellloooooo");
+
+    }
+
+     public static void main(String[] args) {
+
+         System.out.println("NAMASTAAAA");
+     }
+}
+
+ 
