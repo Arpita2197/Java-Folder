a8bcb475a308b9c979f0d6b6e7968f75155ff1e4
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-20 10:15:13 +0530

    Added folder 52

---
+++ 52_enum/T.java
@@ -0,0 +1,17 @@
+enum Days {
+
+     SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY
+}
+
+class T extends Days {
+
+
+}
+
+//  T.java:6: error: cannot inherit from final Days
+// class T extends Days {
+//                 ^
+// T.java:6: error: enum types are not extensible
+// class T extends Days {
+// ^
+// 2 errors
