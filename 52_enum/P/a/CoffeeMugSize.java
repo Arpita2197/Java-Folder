dbcd4c69f5ff5620eb35e040e37f1e1dfcdaca56
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-20 10:17:23 +0530

    Added folder 52

---
+++ 52_enum/P/a/CoffeeMugSize.java
@@ -0,0 +1,6 @@
+package a;
+
+public enum CoffeeMugSize {
+
+     SMALL , MEDIUM , LARGE ;
+}
---
+++ 52_enum/P/b/A.java
@@ -0,0 +1,15 @@
+package b;
+
+import a.CoffeeMugSize;
+
+class A {
+
+    public static void main(String[] args) {
+
+        CoffeeMugSize x = CoffeeMugSize.SMALL;
+
+        System.out.println(x);
+    }
+}
+
+ // it proves that enum constants are public.....
