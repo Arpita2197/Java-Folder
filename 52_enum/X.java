121643b33915675c3f6e7d3cfa91dc4ef47ec01a
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-20 10:16:40 +0530

    Added folder 52

---
+++ 52_enum/X.java
@@ -0,0 +1,18 @@
+enum CC {
+
+     class Z {
+
+
+     }
+
+      static class Y {
+
+         
+      }
+}
+
+//  C:\Users\lenovo\OneDrive\Desktop\java\52_enum>javac X.java
+// X.java:3: error: enum constant expected here
+//      class Z {
+//      ^
+// 1 error
