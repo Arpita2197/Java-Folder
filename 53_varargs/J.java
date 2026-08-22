9f6fee6bea3e690ef24cab5923687b5cd54c35f1
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-22 21:09:49 +0530

    Added folder 53

---
+++ 53_varargs/J.java
@@ -0,0 +1,14 @@
+class J {
+
+    static void pro(String x , int...y) {
+         
+         System.out.println(x);
+          System.out.println(y.length);
+       
+    }
+
+      public static void main(String[] args) {
+
+           pro("om",12,3,4,5,6);
+      }
+}
