1b475728c733666921845c434bf57198108a6efc
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-22 21:09:21 +0530

    Added folder 53

---
+++ 53_varargs/I1.java
@@ -0,0 +1,13 @@
+class I1 {
+
+    static void pro(String x , int...y) {
+         
+         System.out.println("HIIIIIIii");
+       
+    }
+
+      public static void main(String[] args) {
+
+           pro("om");
+      }
+}
