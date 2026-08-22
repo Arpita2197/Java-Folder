05e1d3e1c3daeae2b3f9682adacb519203f38f32
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-22 21:09:30 +0530

    Added folder 53

---
+++ 53_varargs/I2.java
@@ -0,0 +1,14 @@
+class I2 {
+
+    static void pro(String x , int...y) {
+         
+         System.out.println(x);
+          System.out.println(y.length);
+       
+    }
+
+      public static void main(String[] args) {
+
+           pro("om");
+      }
+}
