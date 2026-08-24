ff3c4f674feccfafa9f968197772475565629a5b
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-24 22:08:28 +0530

    Added folder 53

---
+++ 53_varargs/L.java
@@ -0,0 +1,14 @@
+class L {
+
+    static void pro(Animal...x) {
+         
+       System.out.println(x);
+          System.out.println(x.length);
+       
+    }
+
+      public static void main(String[] args) {
+
+           pro();
+      }
+}
