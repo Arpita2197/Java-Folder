a1884211df459f6ebbc85699f2509c0b59cc1c29
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-25 22:14:34 +0530

    Added folder 53

---
+++ 53_varargs/Q.java
@@ -0,0 +1,13 @@
+class Q {
+
+    static void pro(int...marks, String[] x) {
+         
+       
+    }
+     
+}
+
+//  Q.java:3: error: varargs parameter must be the last parameter
+//     static void pro(int...marks, String[] x) {
+//                           ^
+// 1 error
