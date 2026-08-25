78967b6fe7a9f6c5213c8d8df1eca1c358a7468b
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-25 22:13:54 +0530

    Added folder 53

---
+++ 53_varargs/O.java
@@ -0,0 +1,17 @@
+class O {
+
+    static void pro(String...names,int...marks) {
+         
+       
+    }
+
+      public static void main(String[] args) {
+
+           pro("om","ram",12,15);
+      }
+}
+
+//  O.java:3: error: varargs parameter must be the last parameter
+//     static void pro(String...names,int...marks) {
+//                              ^
+// 1 error
