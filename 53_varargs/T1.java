b2b1c3a2ca020770cf845da35b58b008d6443b87
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-26 09:43:55 +0530

    Added folder 53

---
+++ 53_varargs/T1.java
@@ -0,0 +1,20 @@
+class T1 {
+
+     static void pro(int...x) {
+
+         System.out.println("HIIIII");
+     }
+     
+
+    //   static void pro(Integer... x) {
+
+    //      System.out.println("Helloooooooooo");
+    //  }
+
+      public static void main(String[] args) {
+
+         pro(15,29);
+
+      }
+     
+}      
