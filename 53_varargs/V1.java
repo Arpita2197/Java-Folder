878c70b3f9479a4ea84c2e8c3155f9d072282cf6
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-26 09:45:38 +0530

    Added folder 53

---
+++ 53_varargs/V1.java
@@ -0,0 +1,21 @@
+class V1 {
+
+     static void pro(String......x) {
+
+         System.out.println("HIIIII");
+     }
+    
+      public static void main(String[] args) {
+             
+        String[][] y = {{"mohan","om"},{"sonu","monu","dinesh"}};
+
+         pro(y);
+
+      }
+     
+}  
+
+//  V1.java:3: error: <identifier> expected
+//      static void pro(String......x) {
+//                               ^
+// 1 error
