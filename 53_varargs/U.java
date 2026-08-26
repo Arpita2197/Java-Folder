83a41769863b07c7bf088fca50e9cb815ae9e906
Arpita2197 <arpitasahu2197@gmail.com>
2026-08-26 09:44:13 +0530

    Added folder 53

---
+++ 53_varargs/U.java
@@ -0,0 +1,20 @@
+class U {
+
+     static void pro(int...x) {
+
+         System.out.println("HIIIII");
+     }
+     
+
+      static void pro(Integer... x) {
+
+         System.out.println("Helloooooooooo");
+     }
+
+      public static void main(String[] args) {
+
+         pro(new int[] {15,29,22});
+
+      }
+     
+}       
