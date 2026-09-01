970dc77fde61e037946d797e9e9976e140ef8471
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-01 23:45:45 +0530

    Added folder 55

---
+++ 55_reflection/G.java
@@ -0,0 +1,15 @@
+class G {
+
+     public static void main(String[] args) {
+
+           Class klass = EE.D.class;
+
+        Class kl = klass.getEnclosingClass();
+
+        System.out.println(kl.getName());
+
+      }    
+           
+  }
+
+    //  getEnclosingClass() 	==> Returns the immediately enclosing class of the underlying class.
