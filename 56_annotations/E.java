a2db474a05c4745211f05160f11037eb7ca854b2
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-03 23:05:23 +0530

    Added folder 56

---
+++ 56_annotations/E.java
@@ -0,0 +1,14 @@
+class E {
+
+    public static void main(String[] args) {
+          
+          Integer x = new Integer(15);
+          System.out.println(x);
+   
+    }
+}
+
+//  E.java:5: warning: [removal] Integer(int) in Integer has been deprecated and marked for removal
+//           Integer x = new Integer(15);
+//                       ^
+// 1 warning
