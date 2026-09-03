9da608a78bff19e07565cd4b429b95be5b036724
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-03 23:07:10 +0530

    Added folder 56

---
+++ 56_annotations/G.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class G {
+
+    public static void main(String[] args) {
+
+         ArrayList a = new ArrayList();
+
+         a.add(12);
+         a.add(14);
+
+          System.out.println(a);
+    }
+}
+
+//  Note: G.java uses unchecked or unsafe operations.
+// Note: Recompile with -Xlint:unchecked for details.
