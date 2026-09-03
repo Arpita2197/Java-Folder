e1740e155d37e202ccc0b788d9390ca570efe5b9
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-03 23:07:29 +0530

    Added folder 56

---
+++ 56_annotations/H.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class H {
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+
+         ArrayList a = new ArrayList();
+
+         a.add(12);
+         a.add(14);
+
+          System.out.println(a);
+    }
+}
