dd215cf9df480fb9c9f2d5d9a1f4f04fda2ac82e
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-03 23:08:03 +0530

    Added folder 56

---
+++ 56_annotations/K.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+
+   @SuppressWarnings("unchecked")
+class K {
+      
+    //    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+
+         ArrayList a = new ArrayList();
+
+         a.add(12);
+         a.add(14);
+
+          System.out.println(a);
+    }
+     
+
+        //   @SuppressWarnings("unchecked")
+     static void pro() {
+
+          ArrayList x = new ArrayList();     
+                                                 
+         x.add(10);
+         x.add(11);
+
+          System.out.println(x);
+     }
+} 
