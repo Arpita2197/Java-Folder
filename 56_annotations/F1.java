0645b15136b336aa84d1aac65777e53358872cb7
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-03 23:05:43 +0530

    Added folder 56

---
+++ 56_annotations/F1.java
@@ -0,0 +1,10 @@
+class F1 {
+
+    public static void main(String[] args) {
+           
+           @SuppressWarnings("removal")
+          Integer x = new Integer(15);
+          System.out.println(x);
+   
+    }
+}
