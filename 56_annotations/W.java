0bfc11748de7871e6e321b59d39dbb7349b69a62
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-06 22:51:43 +0530

    Added folder 56

---
+++ 56_annotations/W.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+@Deprecated
+class W1 {
+
+   @Deprecated
+   static int y ;
+}
+
+class W {
+
+       @SuppressWarnings("unchecked")
+       @SuppressWarnings("deprecation")
+
+    public static void main(String[] args) {
+   
+        //  @SuppressWarnings("deprecation")
+         W1 x;
+
+         ArrayList a = new ArrayList();
+       
+         a.add(15);
+
+          System.out.println(W1.y);
+
+
+    }
+}
+
+//  W.java:13: error: SuppressWarnings is not a repeatable annotation type
+//        @SuppressWarnings("deprecation")
+//        ^
+// Note: W.java uses or overrides a deprecated API.
+// Note: Recompile with -Xlint:deprecation for details.
+// Note: W.java uses unchecked or unsafe operations.
+// Note: Recompile with -Xlint:unchecked for details.
+// 1 error
