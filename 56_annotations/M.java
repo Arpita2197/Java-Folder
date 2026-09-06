e70039db7153933f263a112c3fd974c9c274d054
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-06 22:47:58 +0530

    Added folder 56

---
+++ 56_annotations/M.java
@@ -0,0 +1,13 @@
+class M {
+
+
+      @Deprecated
+      static int y ;
+
+     public static void main(String[] args) {
+
+         System.out.println(y);
+     }
+}
+
+ // deprecated ko khud ki class ma use kar raha toh warning nhe dega.....
