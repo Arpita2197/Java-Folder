a34eb69f40ca709232d1e075f1edecbf71ecf224
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-06 22:52:01 +0530

    Added folder 56

---
+++ 56_annotations/Y.java
@@ -0,0 +1,19 @@
+
+@FunctionalInterface
+interface Y {
+
+
+}
+
+//  Y.java:2: error: Unexpected @FunctionalInterface annotation
+// @FunctionalInterface
+// ^
+//   Y is not a functional interface
+//     no abstract method found in interface Y
+// 1 error
+
+// C:\Users\lenovo\OneDrive\Desktop\java\56_annotations>
+
+
+ // FunctionalInterface batana ka liya apko ek bss method declaration karna hota ha....
+  // ek method declaration jarauri ha interface ko functional inteface batana ka liya....
