7ffb62d97832c5033707c98523970578c48605d8
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-06 22:52:09 +0530

    Added folder 56

---
+++ 56_annotations/Z.java
@@ -0,0 +1,15 @@
+
+@FunctionalInterface
+interface Z {
+
+ void pro();
+ void info();
+
+}
+
+//  Z.java:2: error: Unexpected @FunctionalInterface annotation
+// @FunctionalInterface
+// ^
+//   Z is not a functional interface
+//     multiple non-overriding abstract methods found in interface Z
+// 1 error
