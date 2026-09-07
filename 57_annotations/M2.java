64eab02538a791f178d8b1f9bd001628518450c9
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-07 23:14:56 +0530

    Added folder 57

---
+++ 57_annotations/M2.java
@@ -0,0 +1,11 @@
+import  static java.lang.Math.*;
+
+class M2 {
+
+     public static void main(String[] args) {
+
+         System.out.println(PI);
+          System.out.println(max(15,12));
+            System.out.println(random());
+     }
+}
