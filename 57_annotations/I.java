bfbbd06dc82ec9698d14f326e7ee026730e27d46
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-07 23:13:36 +0530

    Added folder 57

---
+++ 57_annotations/I.java
@@ -0,0 +1,26 @@
+@interface MNO {
+
+
+}
+
+class I {
+
+    @MNO 
+    {
+
+    }
+
+    @MNO 
+    static {
+
+
+    }
+}
+
+//  I.java:9: error: illegal start of type
+//     {
+//     ^
+// I.java:14: error: illegal start of type
+//     static {
+//            ^
+// 2 errors
