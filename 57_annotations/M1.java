31d8798abcb9eed1e6960516a4da18e9a8cd3039
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-07 23:14:49 +0530

    Added folder 57

---
+++ 57_annotations/M1.java
@@ -0,0 +1,9 @@
+class M1 {
+
+     public static void main(String[] args) {
+
+         System.out.println(Math.PI);
+          System.out.println(Math.max(15,12));
+            System.out.println(Math.random());
+     }
+}
