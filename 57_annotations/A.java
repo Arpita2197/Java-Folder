2a75644d79bd29aef745a866d7490b9d2820e035
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-07 23:10:44 +0530

    Added folder 567

---
+++ 57_annotations/A.java
@@ -0,0 +1,20 @@
+     
+       // ham khud ka annotations bana raha hai......
+
+ interface AA {
+
+
+ }      
+
+ @AA
+ class A {
+
+
+
+ }
+
+
+//  A.java:9: error: incompatible types: AA cannot be converted to Annotation
+//  @AA
+//   ^
+// 1 error
