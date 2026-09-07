ee2873dcd68cb5d4788f4b5662a2964eb1560614
Arpita2197 <arpitasahu2197@gmail.com>
2026-09-07 23:12:38 +0530

    Added folder 57

---
+++ 57_annotations/E1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+class E1 {
+
+     @SuppressWarnings({"removal","deprecation","unchecked"})
+    public static void main(String[] args) {
+
+        Integer a = new Integer(15);
+
+        ArrayList x = new ArrayList();
+
+        x.add(29);
+
+        Date dt = new Date(2005,3,29);
+    }
+}
